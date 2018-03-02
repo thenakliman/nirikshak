81d77ac4f2e66f2d5c696d321013e050856935d1
Rajiv Kumar <thenakliman@gmail.com>
2018-03-02 17:26:13 +0530

    Refactored dump_csv output module

--- nirikshak/output/dump_csv.py
+++ nirikshak/output/dump_csv.py
@@ -23,21 +23,31 @@ LOG = logging.getLogger(__name__)
 
 @base.register('csv')
 class CSVFormatOutput(base.FormatOutput):
-    def output(self, **kwargs):
+    @staticmethod
+    def _get_output_file_location():
+        output_file_loc = None
         try:
-            out_file = nirikshak.CONF['output_csv']['output_dir']
+            output_file_loc = nirikshak.CONF['output_csv']['output_dir']
         except KeyError:
-            out_file = '/var/lib/nirikshak/result.csv'
+            output_file_loc = '/var/lib/nirikshak/result.csv'
 
-        output_file = []
+        return output_file_loc
+
+    @staticmethod
+    def _read_csv_file(file_location):
+        output_file_content = []
         try:
-            with open(out_file, 'r') as fobj:
+            with open(file_location, 'r') as fobj:
                 reader = csv.reader(fobj, delimiter=' ', quotechar='|')
                 for row in reader:
-                    output_file.append(row)
+                    output_file_content.append(row)
         except IOError:
             pass
 
+        return output_file_content
+
+    @staticmethod
+    def _format_output_for_csv(**kwargs):
         key = list(kwargs.keys())[0]
         try:
             expected_result = kwargs[key]['output']['result']
@@ -51,10 +61,19 @@ class CSVFormatOutput(base.FormatOutput):
                 jaanch, expected_result,
                 kwargs[key]['input'].get('result')))
 
-        output_file.append([jaanch])
-        with open(out_file, "w") as csv_file:
+        return jaanch
+
+    @staticmethod
+    def _write_csv_file(content, file_location):
+        with open(file_location, "w") as csv_file:
             csv_writer = csv.writer(csv_file)
-            for row in output_file:
+            for row in content:
                 csv_writer.writerow(row[0].split(','))
 
-        LOG.info("Output has been dumped in %s file", out_file)
+    def output(self, **kwargs):
+        output_file_location = self._get_output_file_location()
+        output_file_content = self._read_csv_file(output_file_location)
+        formatted_output = self._format_output_for_csv(**kwargs)
+        output_file_content.append([formatted_output])
+        self._write_csv_file(output_file_content, output_file_location)
+        LOG.info("Output has been dumped in %s file", output_file_location)
