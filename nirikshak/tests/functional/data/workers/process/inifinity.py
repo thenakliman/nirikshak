55e4e66aa64e50a99fb29cd433605bd8d8957be4
Rajiv Kumar <thenakliman@gmail.com>
2018-03-26 22:27:19 +0530

    Added new data for functional test case

--- nirikshak/tests/functional/data/workers/process/inifinity.py
+++ nirikshak/tests/functional/data/workers/process/inifinity.py
@@ -1,2 +1,18 @@
+#!/usr/bin/python
+import socket
+import sys
+
+
+HOST = 'localhost'
+PORT = 5000
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+    s.bind((HOST, PORT))
+except socket.error, msg:
+    sys.exit()
+
+s.listen(10)
+conn = None
 while True:
-    pass
+    conn, addr = s.accept()
+    conn.close()
